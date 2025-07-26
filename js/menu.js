a88b813b747d35ab088d2a8ec5d28036b9ed9e0a
Aastha0625 <justofficial06@gmail.com>
2025-07-26 11:15:01 +0530

    UI- fixes on about us and hero pages

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tl = gsap.timeline({ paused: true });
   const currentTime = document.querySelector("#currentTime");
   const menuToggleBtn = document.querySelector(".menu");
   const menuItemsContainer = document.querySelector(".menu-container");
@@ -7,71 +6,47 @@ document.addEventListener("DOMContentLoaded", function () {
   let isMenuOpen = false;
 
   gsap.set(menuItemsContainer, { y: 50, opacity: 0 });
-  gsap.set(menuItems, { y: 50, opacity: 0 });
+  gsap.set(menuItems, { y: 50, opacity: 0, pointerEvents: "none" });
 
-  function time() {
-    var d = new Date();
-    var s = d.getSeconds();
-    var m = d.getMinutes();
-    var h = d.getHours();
-    currentTime.textContent =
-      ("0" + h).substr(-2) +
-      ":" +
-      ("0" + m).substr(-2) +
-      ":" +
-      ("0" + s).substr(-2);
+  function updateClock() {
+    const now = new Date();
+    currentTime.textContent = now
+      .toLocaleTimeString("en-GB", { hour12: false }); // or customize as before
   }
-
-  setInterval(time, 1000);
+  setInterval(updateClock, 1000);
 
   function toggleMenu() {
     if (!isMenuOpen) {
       menuToggleBtn.classList.add("active");
 
-      gsap.to(menuItemsContainer, {
+      gsap.to(menuItemsContainer, { y: 0, opacity: 1, duration: 0.5, ease: "power2.out" });
+      gsap.to(menuItems, {
         y: 0,
         opacity: 1,
+        pointerEvents: "all",
         duration: 0.5,
+        stagger: 0.05,
         ease: "power2.out",
       });
 
-      gsap.to(
-        menuItems,
-        {
-          pointerEvents: "all",
-          y: 0,
-          opacity: 1,
-          duration: 0.5,
-          stagger: 0.05,
-          ease: "power2.out",
-          onComplete: () => {
-            isMenuOpen = true;
-          },
-        },
-        "<"
-      );
+      isMenuOpen = true;
     } else {
-      if (isMenuOpen) {
-        menuToggleBtn.classList.remove("active");
+      menuToggleBtn.classList.remove("active");
 
-        gsap.to(menuItemsContainer, {
-          y: -50,
-          opacity: 0,
-          duration: 0.5,
-          ease: "power2.out",
-          onComplete: () => {
-            gsap.set(menuItemsContainer, { y: 50, opacity: 0 });
-            gsap.set(menuItems, { y: 50, opacity: 0, pointerEvents: "none" });
-            isMenuOpen = false;
-          },
-        });
-      }
+      gsap.to(menuItemsContainer, {
+        y: -50,
+        opacity: 0,
+        duration: 0.5,
+        ease: "power2.out",
+        onComplete: () => {
+          gsap.set(menuItemsContainer, { y: 50, opacity: 0 });
+          gsap.set(menuItems, { y: 50, opacity: 0, pointerEvents: "none" });
+          isMenuOpen = false;
+        },
+      });
     }
   }
 
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener("click", toggleMenu);
-  });
-
+  menuItems.forEach(item => item.addEventListener("click", toggleMenu));
   menuToggleBtn.addEventListener("click", toggleMenu);
 });

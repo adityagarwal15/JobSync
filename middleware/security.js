232968914a2c3b5117ad9ccf020736d5ccbe0660
100NikhilBro <100pranjalgupta@gmail.com>
2025-09-09 23:49:18 +0530

    fix(security): add helmet and hpp middleware to mitigate parameter pollution (#318)

---
+++ middleware/security.js
@@ -0,0 +1,20 @@
+const helemt = require('helmet');
+const hpp = require('hpp');
+
+
+exports.securityMiddleware = (app) => {
+
+
+    app.use(helemt({
+        contentSecurityPolicy: false,
+        noSniff: true,
+        frameguard: { action: 'sameorigin' },
+        hidePoweredBy: true,
+        referrerPolicy: { policy: "no-referrer" },
+        crossOriginEmbedderPolicy: false,
+        crossOriginResourcePolicy: { policy: "cross-origin" },
+        crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" },
+    }))
+
+    app.use(hpp());
+}
